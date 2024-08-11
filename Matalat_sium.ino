f80c189acb1bf730930cdb5bf89e6056d8eebb80
itai1989 <itait1989@gmail.com>
2024-08-11 17:55:56 +0300

    הגדרת פינים ומערכים

---
+++ Matalat_sium.ino
@@ -0,0 +1,35 @@
+#define pinLed_G 6
+#define pinLed_Y 9
+#define pinLed_B 10
+#define pinLed_R 11
+
+#define pinBtn_G 1
+#define pinBtn_Y 2
+#define pinBtn_B 3
+#define pinBtn_R 4
+
+#define Speaker 8
+
+#define NUM_OF_LEDS 4
+int Leds[NUM_OF_LEDS] = { pinLed_G, pinLed_Y, pinLed_B, pinLed_R };
+int Btns[NUM_OF_LEDS] = { pinBtn_G, pinBtn_Y, pinBtn_B, pinBtn_R };
+int isLedON[NUM_OF_LEDS] = { false, false, false, false };
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
