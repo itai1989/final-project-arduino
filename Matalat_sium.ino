92ac1579a5b588f495e3c72c01005bff2f930cf1
itai1989 <itait1989@gmail.com>
2024-08-11 18:41:40 +0300

    הוספת פונקציות להדלקת נורות

--- Matalat_sium.ino
+++ Matalat_sium.ino
@@ -14,22 +14,44 @@
 int Leds[NUM_OF_LEDS] = { pinLed_G, pinLed_Y, pinLed_B, pinLed_R };
 int Btns[NUM_OF_LEDS] = { pinBtn_G, pinBtn_Y, pinBtn_B, pinBtn_R };
 int isLedON[NUM_OF_LEDS] = { false, false, false, false };
+int rnd;
 
 void setup() {
-  // put your setup code here, to run once:
-
+  for (int k = 0; k < NUM_OF_LEDS; k++) {
+    pinMode(Leds[k], OUTPUT);
+  }
+  for (int k = 0; k < NUM_OF_LEDS; k++) {
+    pinMode(Btns[k], INPUT_PULLUP);
+  }
+  randomSeed(analogRead(A1));
+  pinMode(Speaker, OUTPUT);
+  playGame();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
 }
-void setup() {
-  // put your setup code here, to run once:
-
+void playGame(){
+  rndLeds();
+  ledsBlink();
 }
-
-void loop() {
-  // put your main code here, to run repeatedly:
-
+void rndLeds() {
+  for (int i = 0; i < 3; i++) {
+    rnd = random(0, 4);
+    while (isLedON[rnd]) {
+      rnd = random(0, 4);
+    }
+    isLedON[rnd] = true;
+  }
+}
+void ledsBlink() {
+  for (int i = 0; i < isLedON; i++) {
+    if (isLedON[i]) {
+      LedBlink(i);
+    }
+  }
+}
+void LedBlink(int chnl) {
+  digitalWrite(Leds[chnl], HIGH);
+  delay(1000);
 }
